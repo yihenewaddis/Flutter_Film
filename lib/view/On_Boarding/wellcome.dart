8379517ee5c8e35be5d1ea3c3d305def1ec323e0
yihenewaddis <yihenewu@gmail.com>
2024-02-20 15:59:08 +0300

    i went to add onBoarding page

---
+++ lib/view/On_Boarding/wellcome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WellComeScreen extends StatelessWidget {
+  const WellComeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('well come page and i am yihenew')),
+    );
+  }
+}
